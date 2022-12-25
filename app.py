5cf9f15e1947b77128b3f5ad3e1128df59458810
Hunk0724 <hunk123321123@gmail.com>
2022-12-26 06:26:14 +0800

    final

--- app.py
+++ app.py
@@ -1,120 +1,185 @@
-import os
-import sys
-
-from flask import Flask, jsonify, request, abort, send_file
+# # -*- coding: utf-8 -*-
+# #載入LineBot所需要的套件
+from flask import Flask, request, abort, send_file
 from dotenv import load_dotenv
-from linebot import LineBotApi, WebhookParser
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
+import json,requests
+import random
 from fsm import TocMachine
-from utils import send_text_message
-
+import os
 load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=["idle", "weather", "city"],
     transitions=[
         {
             "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
+            "source": "idle",
+            "dest": "weather",
+            "conditions": "is_going_to_weather",
         },
-        {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
+        {  "trigger": "advance",
+            "source": "weather",
+            "dest" : "city", 
+            "conditions" : "is_going_to_city"
         },
-        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
+        {   "trigger": "advance",
+            "source": "idle",
+            "dest" : "fortune",
+            "conditions":"is_going_to_fortune"
+        },
+        {"trigger": "go_back", "source": ["weather", "city", "fortune"], "dest": "idle"},
     ],
-    initial="user",
+    initial="idle",
     auto_transitions=False,
     show_conditions=True,
 )
+app = Flask(__name__)
 
-app = Flask(__name__, static_url_path="")
-
-
-# get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
-if channel_secret is None:
-    print("Specify LINE_CHANNEL_SECRET as environment variable.")
-    sys.exit(1)
-if channel_access_token is None:
-    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
-    sys.exit(1)
-
+# 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+# 必須放上自己的Channel Secret
+handler = WebhookHandler(channel_secret)
 
+region=""
+state="idle"
 
-@app.route("/callback", methods=["POST"])
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers["X-Line-Signature"]
+        
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # parse webhook body
+    
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    return 'OK'
+cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+def get(city):
+    token = 'CWB-F1343576-D288-4C36-A25C-383E72A80481'
+    url = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=' + token + '&format=JSON&locationName=' + str(city)
+    Data = requests.get(url)
+    #Python 的 JSON 套件將字串轉換成 dict
+    Data = (json.loads(Data.text,encoding='utf-8'))['records']['location'][0]['weatherElement']
+    res = [[] , [] , []]
+    for j in range(3):
+        for i in Data:
+            res[j].append(i['time'][j])
+    return res
 
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
+#訊息傳遞區塊
+line_bot_api.push_message('Uc58783194ae42125c3e691cbeedb84b1', TextSendMessage(text='美好的一天從現在開始!\n想知道甚麼資訊呢?\n輸入:\n 天氣\n 運勢\n 試試看吧!'))
 
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    global state,region
+    
+    reply_token = event.reply_token
+    message =text= event.message.text
+    if re.match(message, '天氣') or re.match(state,'weather')or re.match(state,'city'):
+        if re.match(state,'idle'):
+            state="weather"
+          
+            line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇地區:\n 北部\n 中部\n 南部\n 東部\n 外島'))
+        elif re.match(state,'weather'):
+            if re.match(message, '北部'):
+                region="北部"
+                state='city'
+        
+                line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇縣市:\n 基隆市\n 臺北市\n 新北市\n 新竹市\n 新竹縣\n 桃園縣\n 苗栗縣'))
+            elif re.match(message, '中部'):
+                region="中部"
+                state='city'
+               
+                line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇縣市:\n 臺中市\n 彰化縣\n 南投縣\n 雲林縣\n 嘉義市\n 嘉義縣'))
+            elif re.match(message, '南部'):
+                region="南部"
+                state='city'
+         
+                line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇縣市:\n 台南市\n 高雄市\n 屏東縣'))
+            elif re.match(message, '東部'):
+                region="東部"
+                state='city'
+            
+                line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇縣市:\n 宜蘭縣\n 花蓮縣\n 臺東縣')) 
+            elif re.match(message, '外島'):
+                region="南部"
+                state='city'
+               
+                line_bot_api.reply_message(reply_token,TextSendMessage(text='選擇縣市:\n 澎湖縣\n 金門縣\n 連江縣'))  
+            else :
+                    region=""
+                    state="idle"
+                    line_bot_api.reply_message(reply_token,TextSendMessage(text="格式錯誤，重新輸入:\n 天氣\n 運勢"))
+        elif re.match(state,'city'):
+            if message != "":
+                city = message[0:3]
 
-    return "OK"
+                city = city.replace('台','臺')
+                if(not (city in cities)):
+                    region=""
+                    state="idle"
+                    line_bot_api.reply_message(reply_token,TextSendMessage(text="格式錯誤，重新輸入:\n 天氣\n 運勢"))
+                else:
+                    
+                    res = get(city)
+                    region=""
+                    state="idle"
+                    line_bot_api.reply_message(reply_token, TemplateSendMessage(
+                        alt_text = city + '未來 36 小時天氣預測',
+                        template = CarouselTemplate(
+                            columns = [
+                                CarouselColumn(
+                                    thumbnail_image_url = 'https://i.imgur.com/Ex3Opfo.png',
+                                    title = '{} ~ {}'.format(data[0]['startTime'][5:-3],data[0]['endTime'][5:-3]),
+                                    text = '{}\n天氣狀況 {}\n溫度 {} ~ {} °C\n降雨機率 {}'.format(city,data[0]['parameter']['parameterName'],data[2]['parameter']['parameterName'],data[4]['parameter']['parameterName'],data[1]['parameter']['parameterName']),
+                                    actions = [
+                                        URIAction(
+                                            label = '詳細內容',
+                                            uri = 'https://www.cwb.gov.tw/V8/C/W/County/index.html'
+                                        )
+                                    ]
+                                )for data in res
+                            ]
+                        )
+                    ))
+                    
+                   
+            else: 
+                    line_bot_api.reply_message(reply_token, TextSendMessage(text="尚未選擇縣市，重新輸入:\n 天氣\n 運勢")) 
+                    region=""
+                    state="idle"
+    elif re.match(message,'運勢'):
+        fortune = random.choice(['大凶', '凶', '末吉', '吉','中吉','大吉'])
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="今日運勢: "+fortune))  
+    else :
+        
+            state = "idle"
+            region=""
+            line_bot_api.reply_message(reply_token,TextSendMessage(text='輸入錯誤! 請重新輸入:\n 天氣\n 運勢'))
 
-
-@app.route("/webhook", methods=["POST"])
-def webhook_handler():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-        if not isinstance(event.message.text, str):
-            continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
-        response = machine.advance(event)
-        if response == False:
-            send_text_message(event.reply_token, "Not Entering any State")
-
-    return "OK"
-
-
-@app.route("/show-fsm", methods=["GET"])
+@app.route("/show_fsm", methods=['POST'])            
 def show_fsm():
     machine.get_graph().draw("fsm.png", prog="dot", format="png")
-    return send_file("fsm.png", mimetype="image/png")
-
+    return send_file("fsm.png", mimetype="image/png")                
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
--- fsm.py
+++ fsm.py
@@ -1,36 +1,26 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message
-
-
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
-
-    def is_going_to_state1(self, event):
+    
+    def is_going_to_weather(self, event):
         text = event.message.text
-        return text.lower() == "go to state1"
+        return text.lower() == "go to weather"
 
-    def is_going_to_state2(self, event):
+    def is_going_to_city(self, event):
         text = event.message.text
-        return text.lower() == "go to state2"
+        return text.lower() == "go to city"
 
-    def on_enter_state1(self, event):
-        print("I'm entering state1")
+    def is_going_to_fortune(self, event):
+        text = event.message.text
+        return text.lower() == "go to fortune"
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
-        self.go_back()
+    def on_exit_weather(self):
+        print("Leaving weather")
 
-    def on_exit_state1(self):
-        print("Leaving state1")
+    def on_exit_fortune(self, event):
+        print("Leaving fortune")
 
-    def on_enter_state2(self, event):
-        print("I'm entering state2")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
-        self.go_back()
-
-    def on_exit_state2(self):
-        print("Leaving state2")
+    def on_exit_city(self):
+        print("Leaving city")
